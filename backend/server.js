84e7f1afc1705b42caa2a8960daf1d0753a65ca2
Aditya Kumar Choudhary <kaditya39546@gmail.com>
2025-10-19 19:50:04 +0530

    Modified server.js

--- backend/server.js
+++ backend/server.js
@@ -32,5 +32,5 @@ app.get("*", (req, res) => {
 
 // Start server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`🚀 Server running on port http://localhost:${PORT}`));
 
